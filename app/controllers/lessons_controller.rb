afd8be895443327d1dbd0629e9c3a22bd4b3a30a
Your Name <you@example.com>
2019-11-14 02:49:04 +0000

    Lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_in_current_lesson, only: [:show]
+
   def show
   end
 
   private
+
+  def require_enrolled_in_current_lesson
+    course = current_lesson.section.course
+    if !current_lesson.enrolled_in?(course)
+      redirect_to course_path(course), alert: 'Error Message Here'
+    end
+  end    
 
   helper_method :current_lesson
   def current_lesson
